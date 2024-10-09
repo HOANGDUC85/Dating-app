9f0bd41a08747f043584dfc93813cfe81711c680
DucHD3 <hoangduc22k2002@gmail.com>
2024-10-09 16:14:04 +0700

    s

--- src/services/profile-service.js
+++ src/services/profile-service.js
@@ -5,16 +5,16 @@ export async function loadAllProfiles() {
   try {
     const response = await instance.get(`/profiles`, {
       headers: {
-        Authorization: `Bearer ${localStorage.getItem('userToken')}`,  // Gửi token trong header
-      }
+        Authorization: `Bearer ${localStorage.getItem("userToken")}`, // Gửi token trong header
+      },
     });
-     // Đảm bảo ánh xạ đúng trường hình ảnh
-     return response.data.data.map(profile => ({
+    // Đảm bảo ánh xạ đúng trường hình ảnh
+    return response.data.data.map((profile) => ({
       ...profile,
-      imageUrl: profile.avatar || profile.imageUrl // Sử dụng avatar hoặc imageUrl tuỳ thuộc vào API
+      imageUrl: profile.avatar || profile.imageUrl, // Sử dụng avatar hoặc imageUrl tuỳ thuộc vào API
     }));
   } catch (error) {
-    console.error('Error loading all profiles:', error);
+    console.error("Error loading all profiles:", error);
     throw error;
   }
 }
